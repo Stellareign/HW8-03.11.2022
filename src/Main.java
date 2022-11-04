486e9d169ea99afc63bceb1ba9ccb32b03481cdb
Stellareign <tuguran@mail.ru>
2022-11-04 15:35:53 +0300

    Домашка 8, часть 2, циклы

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Домашка 8, часть2, задача 1, WHILE накопления с процентами, без капитализации:");
-        int deposit = 15_000;
+        System.out.println("Домашка 8, часть 2, задача 1, WHILE накопления с процентами, без капитализации:");
+        int deposit = 15000;
         double saving = 0;
         double percentMonth = 1.07;
         int i = 1;
         saving = deposit + deposit * percentMonth;
-        while (saving <= 12_000_000) {
+        while (saving <= 12000_000) {
             saving = saving + deposit * percentMonth;
-            System.out.print("Месяц " + (i++) + " - сумма накоплений равна " + saving + ". ");
+            if (saving < 1000_000) {
+                System.out.print("Месяц " + (i++) + " - сумма накоплений равна " + saving + " руб. ");
+            }
+            if (saving > 1000_000) {
+                System.out.print("Месяц " + (i++) + " - сумма накоплений равна " + saving / 1000_000 + " млн. руб. ");
+            }
         }
-
 
     }
 }
