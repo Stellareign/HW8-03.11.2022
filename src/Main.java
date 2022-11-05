1dc3ff1e63728cc91d73bc55c19fc072d2e99e9c
Stellareign <tuguran@mail.ru>
2022-11-05 14:49:37 +0300

    Домашка 8, часть 1, циклы, задача 4

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Домашка 8, часть 2, задача 2, циклы, накопления, вывод каждые полгода:");
-        int deposit = 15000;
-        double saving = 0;
-        double percentMonth = 0.07;
-        saving = deposit + deposit * percentMonth;
-        int month = 0;
-        int periodYears = 9;
-             for (month = 1; month <= 12 * periodYears; month++) {
-                 saving = saving + deposit;
-                if (month % 6 == 0) {
-                    if (saving < 1000_000) {
-                        System.out.print("Месяц " + month + " - сумма накоплений равна " + saving + " руб. ");
-                    } else if (saving > 1000_000) {
-                        System.out.print("Месяц " + month + " - сумма накоплений равна " + saving / 1000_000 + " млн. руб. ");
-                        // Вывод в строку для большей компактности.
-                        // Как сделать, чтобы выводилось меньше знаков после запятой?
+        System.out.println("Домашка 8, часть 2, задача 4, циклы:" +
+                "Написать программу, которая считает дни месяца по датам, определяет, " +
+                "какой день пятница, и выводит сообщение с напоминанием, что нужно " +
+                "подготовить еженедельный отчет.");
+        int firstFriday = 3;
+                for (int monthDay = 1; monthDay <= 31; monthDay++) {
+                    for (int everyFriday = firstFriday; everyFriday <= 31; everyFriday = everyFriday + 7) {
+                        if (monthDay == everyFriday) {
+                            System.out.println("Сегодня пятница, " + monthDay + " число. Необходимо подготовить отчет.");
+                        }
                     }
                 }
-            }
         }
     }
 
